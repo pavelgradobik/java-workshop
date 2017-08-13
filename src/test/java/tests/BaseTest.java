a780ef4033e2e8a3e063df65256b7788caff197d
Sergey Pirogov <sergio_89@ukr.net>
2017-08-13 15:57:49 +0300

    initial

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -15,6 +15,7 @@ public class BaseTest {
         ChromeDriverManager.getInstance().setup();
         FirefoxDriverManager.getInstance().setup();
         Configuration.timeout = 10000;
-        Configuration.baseUrl = "http://35.202.149.166:8086";
+        Configuration.baseUrl = "http://35.192.15.224:8086";
+        Configuration.remote = "http://35.202.239.95:4444/wd/hub";
     }
 }
