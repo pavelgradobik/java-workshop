013934e6b159d0d5d76e88d204aa2062c0c6c5bd
Sergey Pirogov <sergio_89@ukr.net>
2017-08-20 13:08:26 +0300

    .idea delete

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
 import io.github.bonigarcia.wdm.ChromeDriverManager;
 import io.github.bonigarcia.wdm.FirefoxDriverManager;
 import org.testng.annotations.BeforeClass;
@@ -14,7 +15,7 @@ public class BaseTest {
         ChromeDriverManager.getInstance().setup();
         FirefoxDriverManager.getInstance().setup();
 //        Configuration.timeout = 10000;
-//        Configuration.baseUrl = "http://35.192.15.224:8086";
+        Configuration.baseUrl = "http://104.197.245.3:8086";
 //        Configuration.remote = "http://35.202.239.95:4444/wd/hub";
     }
 }
